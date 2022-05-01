811091d1c84a4ab4ecd7071b9b0878627c60bb2a
kadraknb <kadrak02@hotmail.com>
2022-05-01 06:04:58 -0300

    Requisitos 9,10

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
+const lista = document.getElementById('lista-tarefas');
+const texto = document.getElementById('texto-tarefa');
 document.getElementById('criar-tarefa').addEventListener('click', () => {
-  document
-    .getElementById('lista-tarefas')
-    .appendChild(document.createElement('li')).innerText =
-    document.getElementById('texto-tarefa').value;
-  document.getElementById('texto-tarefa').value = '';
+  lista
+    .appendChild(document.createElement('li')).innerText = texto.value;
+  texto.value = '';
 });
-document.getElementById('lista-tarefas').addEventListener('click', () => {
-    document.querySelector('.gray').classList.remove('gray');
-    event.target.className = 'gray';
-  
+lista.addEventListener('click', () => {
+  document.querySelector('.gray').classList.remove('gray');
+  event.target.classList.toggle('gray');
+});
+lista.addEventListener('dblclick', () => {
+  event.target.classList.toggle('completed');
+});
+
+document.getElementById('apaga-tudo').addEventListener('click', () => {
+  lista.innerHTML = '';
+  document.getElementById('save').className = 'gray';
 });
